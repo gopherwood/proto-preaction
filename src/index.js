8db87a9a3eb8c26a80630ec36711318dc80dd6ac
Derek Detweiler <derek.detweiler@gmail.com>
2019-10-30 12:22:08 -0400

    Created basic Platypus folder structure and added dependencies.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,86 @@
+/* global require */
 import './styles.css';
-import 'springroll';
+import platypus from 'platypus';
+
+const packageData = require('../package.json'),
+    config = {
+        levels: {},
+        spriteSheets: {}
+    },
+    flatten = {
+        entities: true,
+        scenes: true
+    },
+    importComponents = function (r) {
+        r.keys().forEach((key) => {
+            var arr = key.split('/'),
+                last = arr.length - 1;
+
+            platypus.components[arr[last].substring(0, arr[last].length - 3)] = r(key).default;
+        });
+    },
+    importJSON = function (r, config) {
+        r.keys().forEach((key) => {
+            var arr = key.split('/'),
+                i = 0,
+                props = config,
+                last = arr.length - 1;
+
+            for (i = 0; i < last; i++) {
+                if (arr[i] !== '.') {
+                    if (!props[arr[i]]) {
+                        props[arr[i]] = {};
+                    }
+
+                    props = props[arr[i]];
+
+                    if (flatten[arr[i]]) {
+                        break;
+                    }
+                }
+            }
+            props[arr[last].replace('.json', '')] = r(key);
+        });
+    };
+
+importComponents(require.context(
+    "./components/", // context folder
+    true, // include subdirectories
+    /.*\.js/ // RegExp
+));
+
+// Base configuration
+importJSON(require.context(
+    "./config/", // context folder
+    true, // include subdirectories
+    /.*\.json/ // RegExp
+), config);
+
+// Sprite Sheets
+importJSON(require.context(
+    "../assets/images/", // context folder
+    true, // include subdirectories
+    /.*\.json/ // RegExp
+), config.spriteSheets);
+
+// levels
+importJSON(require.context(
+    "../assets/levels/", // context folder
+    true, // include subdirectories
+    /.*\.json/ // RegExp
+), config.levels);
+
+//standardizeLevelPoints(config.levels);
+
+const game = new platypus.Game(config, {
+    canvasId: 'stage',
+    display: {
+        clearView: true
+    },
+    audio: 'assets/audio/',
+    images: 'assets/images/',
+    name: packageData.name,
+    version: packageData.version
+});
+
+game.loadScene('title-scene');
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,41 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MergeJsonWebpackPlugin = require("merge-jsons-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const HtmlConfig = require(path.join(__dirname, 'html.config'));
-
 const deploy = path.join(__dirname, 'deploy');
-
 module.exports = env => {
   const plugins = [
     new HtmlWebpackPlugin(HtmlConfig),
+    new MergeJsonWebpackPlugin({
+        "output": {
+            "groupBy": [
+                {
+                    "pattern": "./src/config/**/*.json",
+                    "fileName": "./temp.json"
+                }
+            ]
+        }
+    }),
     new MiniCssExtractPlugin(),
-    new CopyPlugin([{ from: path.join(__dirname + '/static'), to: deploy }])
+    new CopyPlugin([{ from: path.join(__dirname + '/static'), to: deploy }]),
   ];
   return {
     stats: 'errors-only',
-
     mode: env.dev ? 'development' : 'production',
-
+    resolve: {
+        alias: {
+            config: path.join(__dirname, 'node_modules/platypus/src/config', env.dev ? 'development' : 'production')
+        }
+    },
     devServer: {
       contentBase: path.join(__dirname, '/static')
     },
-
     entry: ['@babel/polyfill', path.join(__dirname, '/src/index.js')],
     output: {
       path: deploy
     },
-
     optimization: {
       splitChunks: {
         chunks: 'all'
