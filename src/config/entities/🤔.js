2c09f287823c50b485fee33cdc70b469855cde9c
Derek Detweiler <derek.detweiler@gmail.com>
2021-02-03 17:02:09 -0500

    Mostly working kinda (without interpolation)

--- src/config/entities/🤔.js
+++ src/config/entities/🤔.js
@@ -1,25 +1,67 @@
-import EntityController from '../../components/EntityController.js';
-import {createComponentClass} from 'platypus';
+import {Data, arrayCache, createComponentClass} from 'platypus';
+//import EntityController from '../../components/EntityController.js';
 
 const
     SPEED = 0.2,
+    deepCycle = (a) => {
+        if (Array.isArray(a)) {
+            for (let i = 0; i < a.length; i++) {
+                deepCycle(a[i]);
+            }
+            arrayCache.recycle(a);
+        } else if (a instanceof Data) {
+            for (const key in a) {
+                if (a.hasOwnProperty(key)) {
+                    deepCycle(a[key]);
+                }
+            }
+            a.recycle();
+        }
+    },
     Move = createComponentClass({
         properties: {
-            east: 0,
-            north: 0,
-            south: 0,
-            west: 0
+            saveProperties: [
+                'east',
+                'north',
+                'south',
+                'west',
+                'x',
+                'y'
+            ]
         },
         publicProperties: {
             inputDelay: 0,
             renderDelay: 0,
+            east: 0,
+            north: 0,
+            south: 0,
+            west: 0,
             x: 0,
             y: 0
         },
         initialize: function () {
+            const
+                saveProperties = this.saveProperties;
+
             this.owner.triggerEvent('set-label', {
                 text: `Input: ${this.inputDelay}ms\nRender: ${this.renderDelay}ms`
             });
+
+            // Create editable version of saveProperties
+            this.saveProperties = arrayCache.setUp();
+            if (Array.isArray(saveProperties)) {
+                for (let i = 0; i < saveProperties.length; i++) {
+                    const
+                        saveProperty = saveProperties[i];
+
+                    if (Array.isArray(saveProperty)) {
+                        this.saveProperties.push(saveProperty);
+                    } else {
+                        this.saveProperties.push(saveProperty.split('.'));
+                    }
+                }
+            }
+            this.snapshots = Data.setUp();
         },
         events: {
             "handle-logic": function (tick) {
@@ -40,13 +82,98 @@ const
             },
             "west": function (event) {
                 this.west = event.pressed ? 1 : 0;
+            },
+            "delay-east": function (event) {
+                if (event.triggered || event.released) {
+                    this.owner.triggerHistoricalEvent(this.owner.parent.timestamp + this.inputDelay, "east", Data.setUp(
+                        'pressed', event.pressed,
+                        'released', event.released,
+                        'triggered', event.triggered
+                    ));
+                }
+            },
+            "delay-north": function (event) {
+                if (event.triggered || event.released) {
+                    this.owner.triggerHistoricalEvent(this.owner.parent.timestamp + this.inputDelay, "north", Data.setUp(
+                        'pressed', event.pressed,
+                        'released', event.released,
+                        'triggered', event.triggered
+                    ));
+                }
+            },
+            "delay-south": function (event) {
+                if (event.triggered || event.released) {
+                    this.owner.triggerHistoricalEvent(this.owner.parent.timestamp + this.inputDelay, "south", Data.setUp(
+                        'pressed', event.pressed,
+                        'released', event.released,
+                        'triggered', event.triggered
+                    ));
+                }
+            },
+            "delay-west": function (event) {
+                if (event.triggered || event.released) {
+                    this.owner.triggerHistoricalEvent(this.owner.parent.timestamp + this.inputDelay, "west", Data.setUp(
+                        'pressed', event.pressed,
+                        'released', event.released,
+                        'triggered', event.triggered
+                    ));
+                }
+            }
+        },
+        methods: {
+            updateProperties: function (fromObj, toObj) {
+                const saveProperties = this.saveProperties;
+
+                for (let i = 0; i < saveProperties.length; i++) {
+                    const depth = saveProperties[i];
+                    let fromDeeperObj = fromObj,
+                        toDeeperObj = toObj,
+                        key = depth[0];
+
+                    for (let j = 1; j < depth.length; j++) {
+                        fromDeeperObj = fromDeeperObj[key];
+                        if (!toDeeperObj[key]) { //TODO: Probably need to add support for arrays and such.
+                            toDeeperObj[key] = Data.setUp();
+                        }
+                        toDeeperObj = toDeeperObj[key];
+                        key = depth[j];
+                    }
+                    toDeeperObj[key] = fromDeeperObj[key];
+                }
+
+                return toObj;
+            }
+        },
+        publicMethods: {
+            discardSnapshot: function (timestamp) {
+                deepCycle(this.snapshots[timestamp]);
+                delete this.snapshots[timestamp];
+            },
+            restoreSnapshot: function (timestamp) {
+                const snapshot = this.snapshots[timestamp];
+
+                if (snapshot) {
+                    return this.updateProperties(snapshot, this.owner);
+                } else {
+                    return null;
+                }
+            },
+            saveSnapshot: function (timestamp) {
+                const snapshot = this.snapshots[timestamp] = this.updateProperties(this.owner, this.snapshots[timestamp] || Data.setUp());
+
+                return snapshot;
+            },
+            triggerHistoricalEvent: function (timestamp, ...args) {
+                this.owner.parent.triggerHistoricalEvent(timestamp, this.owner, ...args);
             }
         }
     });
 
 export default {
     id: "🤔",
-    components: [EntityController, {
+    components: [{
+        type: "EntityController"
+    }, {
         type: "RenderText",
         style: {
             fontSize: "48px",
@@ -70,14 +197,14 @@ export default {
     }, Move],
     properties: {
         controlMap: {
-            ArrowRight: "east",
-            ArrowDown: "south",
-            ArrowLeft: "west",
-            ArrowUp: "north",
-            RightDPad: "east",
-            DownDPad: "south",
-            LeftDPad: "west",
-            UpDPad: "north"
+            ArrowRight: "delay-east",
+            ArrowDown: "delay-south",
+            ArrowLeft: "delay-west",
+            ArrowUp: "delay-north",
+            RightDPad: "delay-east",
+            DownDPad: "delay-south",
+            LeftDPad: "delay-west",
+            UpDPad: "delay-north"
         },
         x: 0,
         y: 0,
---
+++ src/config/scenes/title-scene.js
@@ -0,0 +1,212 @@
+import {Data, arrayCache, createComponentClass} from 'platypus';
+
+const
+    SPEED = 0.2,
+    DelayInputs = createComponentClass({
+        properties: {
+            lifetime: 255
+        },
+        publicProperties: {
+            timestamp: 5
+        },
+        initialize: function () {
+            this.history = [];
+            this.goBackInTime = Infinity;
+        },
+        events: {
+            "tick": function (tick) {
+                const
+                    goBackInTime = this.goBackInTime;
+
+                if (goBackInTime !== Infinity) {
+                    this.goBackInTime = Infinity;
+
+                    // Warning: this method will indirectly call "tick" multiple times in the restoration process
+                    this.restoreSnapshot(goBackInTime);
+                }
+            },
+
+            "handle-logic": function (tick) {
+                const
+                    gameTick = tick.tick;
+
+                this.history.push(Data.setUp(
+                    'timestamp', this.timestamp,
+                    'entities', this.saveSnapshot(tick),
+                    'events', arrayCache.setUp(Data.setUp(//Generate tick for past-future use
+                        'timestamp', this.timestamp,
+                        'entity', this.owner,
+                        'args', arrayCache.setUp('tick', Data.setUp(
+                            'delta', tick.delta,
+                            'deltaMS', tick.delta,
+                            'deltaTime', gameTick.deltaTime,
+                            'elapsed', gameTick.elapsed
+                        ))
+                    ))
+                ));
+
+                this.timestamp += tick.delta;
+
+                while (this.history.length > this.lifetime / tick.delta) {
+                    const snapshot = this.history.shift();
+
+                    this.discardSnapshot(snapshot);
+                }
+            }
+        },
+        methods: {
+            getSnapshotIndex: function (timestamp) {
+                const
+                    history = this.history;
+                let i = 0,
+                    snapshot = history[0];
+
+                while (snapshot && snapshot.timestamp !== timestamp) {
+                    i += 1;
+                    snapshot = history[i];
+                }
+
+                return snapshot ? i : -1;
+            }
+        },
+        publicMethods: {
+            discardSnapshot: function (snapshot) {
+                const
+                    entities = snapshot.entities,
+                    events = snapshot.events;
+
+                for (let i = 0; i < entities.length; i++) {
+                    const entity = entities[i];
+
+                    if (entity.discardSnapshot) {
+                        entity.discardSnapshot(snapshot.timestamp);
+                    }
+                }
+                arrayCache.recycle(entities);
+
+                for (let i = 0; i < events.length; i++) {
+                    events[i].recycle();
+                }
+                arrayCache.recycle(events);
+
+                snapshot.recycle();
+            },
+            restoreSnapshot: function (timestamp) {
+                const
+                    history = this.history,
+                    index = this.getSnapshotIndex(timestamp),
+                    snapshot = history[index];
+
+                if (snapshot) {
+                    const
+                        discardedHistory = arrayCache.setUp(),
+                        entities = snapshot.entities;
+
+                    for (let i = index; i < history.length; i++) {
+                        discardedHistory.push(history[i]);
+                    }
+                    history.length = index; // trash everything to come before we go again (and start pushing new items on to our history).
+
+                    // Prepare entities for a little re-run
+                    for (let i = 0; i < entities.length; i++) {
+                        const entity = entities[i];
+    
+                        if (entity.restoreSnapshot) {
+                            entity.restoreSnapshot(snapshot.timestamp);
+                        }
+                    }
+
+                    // Now lets try all that again!
+                    this.timestamp = timestamp;
+                    for (let i = 0; i < discardedHistory.length; i++) {
+                        const events = discardedHistory[i].events;
+
+                        for (let j = 0; j < events.length; j++) {
+                            const event = events[j];
+
+                            this.owner.triggerHistoricalEvent(0, event.entity, ...event.args);
+                        }
+
+                        this.discardSnapshot(discardedHistory[i]);
+                    }
+                    arrayCache.recycle(discardedHistory);
+                }
+            },
+            saveSnapshot: function (tick) {
+                const
+                    arr = arrayCache.setUp(),
+                    entities = tick.entities;
+
+                for (let i = 0; i < entities.length; i++) {
+                    const entity = entities[i];
+
+                    if (entity.saveSnapshot) {
+                        entity.saveSnapshot(this.timestamp);
+                        arr.push(entity);
+                    }
+                }
+
+                return arr;
+            },
+            triggerHistoricalEvent: function (timestamp = 0, entity = null, ...args) {
+                const
+                    history = this.history;
+
+                if (!timestamp || timestamp === this.timestamp) {
+                    if (args[0] !== 'tick') { // We inject these separately
+                        history[history.length - 1].events.push(Data.setUp(
+                            'timestamp', timestamp,
+                            'entity', entity,
+                            'args', args
+                        ));
+                    }
+                    (entity || this.owner).triggerEvent(...args);
+                } else {
+                    const
+                        index = this.getSnapshotIndex(timestamp);
+
+                    if (index > -1) {
+                        history[index].events.push(Data.setUp(
+                            'timestamp', timestamp,
+                            'entity', entity,
+                            'args', args
+                        ));
+                        this.goBackInTime = Math.min(this.goBackInTime, timestamp);
+                    }
+                }
+            }
+        }
+    });
+
+export default {
+    id: "title-scene",
+    layers: [{
+        id: "title-layer",
+        components: [DelayInputs, {
+            type: "HandlerController"
+        }, {
+            type: "HandlerLogic",
+            alwaysOn: true
+        }, {
+            type: "HandlerRender"
+        }, {
+            type: "Camera",
+            width: 1200,
+            height: 1200,
+            overflow: true
+        }, {
+            type: "EntityContainer",
+            entities: [
+                {type: "🤔", properties: {inputDelay: -250, renderDelay: 125, x: -300, y: -300}},
+                {type: "🤔", properties: {inputDelay: -250, renderDelay: 75, x: -300, y: 0}},
+                {type: "🤔", properties: {inputDelay: -250, renderDelay: 25, x: -300, y: 300}},
+                {type: "🤔", properties: {inputDelay: -150, renderDelay: 125, x: 0, y: -300}},
+                {type: "🤔", properties: {inputDelay: -150, renderDelay: 75, x: 0, y: 0}},
+                {type: "🤔", properties: {inputDelay: -150, renderDelay: 25, x: 0, y: 300}},
+                {type: "🤔", properties: {inputDelay: -50, renderDelay: 125, x: 300, y: -300}},
+                {type: "🤔", properties: {inputDelay: -50, renderDelay: 75, x: 300, y: 0}},
+                {type: "🤔", properties: {inputDelay: -50, renderDelay: 25, x: 300, y: 300}}
+            ]
+        }]
+    }]
+};
